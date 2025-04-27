a08629cea88e55dc51d5c1819eb8aec46272cf10
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-04-27 23:05:48 +0300

    add header and currentWeather

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import './page.scss'
+import './page.scss';
+import Image from 'next/image';
+import CurrentWeather from './components/currentWeather/currentWeather';
 
 export default function Home() {
   return (
-    <div >
-     
+    <div className='main' style={{ backgroundImage: `url('/background/day-rain.png')` }}>
+      <div className="main__header">
+        <div className="main__header-burger">
+          <Image alt='burgerImg' src={'/icon/menu.svg'} width={30} height={30}></Image>
+        </div>
+        <div className="main__header-inp">
+          <Image alt='searchImg' src={'/icon/search.svg'} width={20} height={20}></Image>
+          <input type="text" placeholder='Search'/>
+        </div>
+      </div>
+      <div className="main__content">
+        <CurrentWeather/>
+      </div>
     </div>
   );
 }
