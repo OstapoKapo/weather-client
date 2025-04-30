a54254c44fcfc199a42c542bf309baf71e525c6f
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-04-30 23:21:02 +0300

    fix some styles

---
+++ src/app/components/hoursTemp/hoursTemp.tsx
@@ -0,0 +1,28 @@
+import './hoursTemp.scss';
+import Image from 'next/image';
+
+const hoursData = [1,2,3,4,5,6,7,8,9,10,11]
+
+const HoursTemp = () => {
+    return (
+    <div className='hours-temp'>
+        <p className="hours-temp__header">
+            Sunny conditions will continue through the day with a high of 25°C.
+        </p>
+        <div className="hours-temp__line"></div>
+        <div className="hours-temp__container">
+          {hoursData.map((item, index) => (
+            <div key={index} className="hours-temp__item">
+            <p>Now</p>
+            <div className="hours-temp__item-img">
+              <Image src={'/icon/cloudy-weather.svg'} alt='weatherType' width={30} height={30}></Image>
+            </div>
+            <p>13°</p>
+          </div>
+          ))}
+        </div>
+    </div>
+    )
+}
+
+export default HoursTemp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
 import './page.scss';
 import Image from 'next/image';
 import CurrentWeather from './components/currentWeather/currentWeather';
+import HoursTemp from './components/hoursTemp/hoursTemp';
 
 export default function Home() {
   return (
-    <div className='main' style={{ backgroundImage: `url('/background/day-rain.png')` }}>
+    <div className='main' style={{ backgroundImage: `url('/background/night-clear.png')` }}>
       <div className="main__header">
-        <div className="main__header-burger">
+        <div className="main__burger">
           <Image alt='burgerImg' src={'/icon/menu.svg'} width={30} height={30}></Image>
         </div>
-        <div className="main__header-inp">
+        <div className="main__search-inp">
           <Image alt='searchImg' src={'/icon/search.svg'} width={20} height={20}></Image>
           <input type="text" placeholder='Search'/>
         </div>
       </div>
+      <CurrentWeather/>
       <div className="main__content">
-        <CurrentWeather/>
+        <HoursTemp/>
+        <div className="main__day-temp"></div>
+        <div className="main__map-temp"></div>
       </div>
     </div>
   );
