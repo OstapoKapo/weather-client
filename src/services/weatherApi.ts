b9cc97f70aefa24365c2d5e13aa0e031d79d120d
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-05-16 05:30:24 +0300

    fix bugs

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -2,18 +2,22 @@
 import axios from 'axios'; 
 import {weatherResponse} from '@/types';
 
+const url = process.env.NEXT_PUBLIC_API_URL;
+
+
 export const getWeatherByCity = async (city: string): Promise<weatherResponse> => {
-  const response = await axios.get(`http://localhost:8000/weather/forecast?city=${city}`);
-  return response.data;
+    console.log(url)
+    const response = await axios.get(`${url}weather/forecast?city=${city}`);
+    return response.data;
 };
 
 export const getCityByCoords = async (lat: number, lon: number) => {
-    const response = await axios.get(`http://localhost:8000/weather/city?lat=${lat}&lon=${lon}`);
+    const response = await axios.get(`${url}weather/city?lat=${lat}&lon=${lon}`);
     return response.data[0]?.name;
    
 };
 
 export const fetchCities = async (query: string) => {
-    const response = await axios.get(`http://localhost:8000/weather/fetchCities?query=${query}`);
+    const response = await axios.get(`${url}weather/fetchCities?query=${query}`);
     return response.data;
 }
