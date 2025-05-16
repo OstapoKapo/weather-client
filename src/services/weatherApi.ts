a9645645b49f569367b13944fca731f2b347edac
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-05-16 05:38:58 +0300

    deploy bug

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -2,22 +2,23 @@
 import axios from 'axios'; 
 import {weatherResponse} from '@/types';
 
-const url = process.env.NEXT_PUBLIC_API_URL;
 
 
 export const getWeatherByCity = async (city: string): Promise<weatherResponse> => {
-    console.log(url)
+    const url = process.env.NEXT_PUBLIC_API_URL;
     const response = await axios.get(`${url}weather/forecast?city=${city}`);
     return response.data;
 };
 
 export const getCityByCoords = async (lat: number, lon: number) => {
+    const url = process.env.NEXT_PUBLIC_API_URL;
     const response = await axios.get(`${url}weather/city?lat=${lat}&lon=${lon}`);
     return response.data[0]?.name;
    
 };
 
 export const fetchCities = async (query: string) => {
+    const url = process.env.NEXT_PUBLIC_API_URL;
     const response = await axios.get(`${url}weather/fetchCities?query=${query}`);
     return response.data;
 }
